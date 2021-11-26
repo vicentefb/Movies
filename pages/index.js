a80a2fdc38c86bb72dc093fd7f74e62eb2b1bd4f
vicentefb <chenteferrara@gmail.com>
2021-11-25 18:50:51 -0600

    finished

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Movies.module.css";
 import Movie from "../src/components/Movie";
 import { useEffect, useState } from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 export default function Home({ trendingMovies }) {
   const [searchResults, setSearchResults] = useState([]);
@@ -22,61 +23,68 @@ export default function Home({ trendingMovies }) {
     setSearchResults(movies);
   };
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Movies App</title>
-        <meta name="description" content="Movies app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Movies App</h1>
-      <div>
-        <form onSubmit={search}>
-          <input
-            className="search"
-            name="searchTerm"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            type="text"
-            placeholder="Search for a movie"
-            required
-          />
+    <>
+      <div className="red-background">Movies App</div>
+      <div className="container">
+        <Head>
+          <title>Movies App</title>
+          <meta name="description" content="Movies app" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-          <button
-            className="btn-search"
-            disabled={search === ""}
-            onClick={async () => {
-              let movies = await fetch(
-                `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${searchTerm}&page-1&include_adult=false`
+        <Row>
+          <Col md={6} className="m-auto">
+            <form className="search-form" onSubmit={search}>
+              <input
+                className="search"
+                name="searchTerm"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                type="text"
+                placeholder="Search for a movie"
+                required
+              />
+
+              <button
+                className="btn-search"
+                disabled={search === ""}
+                onClick={async () => {
+                  let movies = await fetch(
+                    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${searchTerm}&page-1&include_adult=false`
+                  );
+                  movies = await movies.json();
+                  movies = await movies.results;
+                  setSearchResults(movies);
+                }}
+              >
+                Search
+              </button>
+            </form>
+          </Col>
+        </Row>
+        <Container>
+          <div className="movie-search-results-grid">
+            {searchResults.map((each, index) => {
+              return (
+                <Movie
+                  key={index}
+                  index={each.id}
+                  title={each.title}
+                  poster_path={each.poster_path}
+                  overview={each.overview}
+                />
               );
-              movies = await movies.json();
-              movies = await movies.results;
-              setSearchResults(movies);
-            }}
-          >
-            Search
-          </button>
-        </form>
+            })}
+          </div>
+        </Container>
       </div>
-      <div className="movie-search-results-grid">
-        {searchResults.map((each, index) => {
-          return (
-            <Movie
-              key={index}
-              index={each.id}
-              title={each.title}
-              poster_path={each.poster_path}
-              overview={each.overview}
-            />
-          );
-        })}
-      </div>
-    </div>
+    </>
   );
 }
 
 export async function getServerSideProps() {
   const data = await fetch(
-    `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.API_KEY}`
+    `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.API_KEY}`
   );
   let trendingMovies = await data.json();
   trendingMovies = await trendingMovies.results;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,21 +2,23 @@ import Image from "next/image";
 
 const Movie = ({ title, index, overview, poster_path }) => {
   const IMAGES_API = "https://image.tmdb.org/t/p/w500/";
-
-  return (
-    <div className="movie" key={index}>
-      <h3>{title}</h3>
-      <div className="movie-img">
-        <Image
-          src={IMAGES_API + poster_path}
-          alt={title}
-          width={300}
-          height={400}
-        />
+  if (title !== "" && title !== undefined) {
+    return (
+      <div className="movie" key={index}>
+        <div className="movie-img">
+          <Image
+            src={IMAGES_API + poster_path}
+            alt={title}
+            width={300}
+            height={400}
+          />
+        </div>
+        <div className="movie-overview">{overview}</div>
       </div>
-      <div className="movie-overview">{overview}</div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Movie;
